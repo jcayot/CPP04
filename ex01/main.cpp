18803b43aa7456b90f3f8df25d7d91c70a1a5ec8
jules <jules.cayot@ik.me>
2024-05-14 12:02:00 +0300

    main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -9,13 +9,13 @@
 int main() {
 	Animal*	animals[10];
 
-	for(int i = 0; i < 5; i++)
+	for (int i = 0; i < 5; i++)
 		animals[i] = new Cat();
 
-	for(int i = 5; i < 10; i++)
+	for (int i = 5; i < 10; i++)
 		animals[i] = new Dog();
 
-	for(int i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 		delete animals[i];
 
 	return 0;
