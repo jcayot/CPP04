8d0a7b440cde48ee3e8e4abaa5df07de76181bf6
jules <jules.cayot@ik.me>
2024-05-14 10:07:22 +0300

    main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,6 +7,16 @@
 #include "WrongCat.hpp"
 
 int main() {
+	Animal*	animals[10];
 
-	return (0);
+	for(int i = 0; i < 5; i++)
+		animals[i] = new Cat();
+
+	for(int i = 5; i < 10; i++)
+		animals[i] = new Dog();
+
+	for(int i = 0; i < 10; i++)
+		delete animals[i];
+
+	return 0;
 }
